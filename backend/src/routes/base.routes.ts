d0ce9a2a4ea5329a0a735e81a7eef4a649136a5d
oriolcortes <oriol.cortesc@gmail.com>
2026-03-24 19:13:26 +0100

    fix: add authRouter to baseRouter

--- backend/src/routes/base.routes.ts
+++ backend/src/routes/base.routes.ts
@@ -4,11 +4,13 @@
 import express, { Router } from 'express';
 import { errorMiddleware } from '../middlewares/error.middleware';
 import { userRouter } from './user.routes';
+import { authRouter } from './auth.routes';
 
 export const baseRouter = Router();
 
 baseRouter.use(express.json());
 
 baseRouter.use('/users', userRouter);
+baseRouter.use('/auth', authRouter);
 
 baseRouter.use(errorMiddleware);
